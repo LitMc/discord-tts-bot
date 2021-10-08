4348e186bd761ab3054716871aaefee04b0f740b
jln <edfjln@gmail.com>
2021-10-08 18:58:32 +0900

    プロジェクト作成

---
+++ src/main/java/jln/hobby/discordttsbot/DiscordTtsBotApplication.java
@@ -0,0 +1,13 @@
+package jln.hobby.discordttsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordTtsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordTtsBotApplication.class, args);
+	}
+
+}
