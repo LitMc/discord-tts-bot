80e172e9c1b99cb353703d8431afbc160aa1b351
jln <edfjln@gmail.com>
2021-10-12 03:34:28 +0900

    不要なクラス削除

--- src/main/java/jln/hobby/discordttsbot/configuration/PlayerConfiguration.java
+++ src/main/java/jln/hobby/discordttsbot/configuration/PlayerConfiguration.java
@@ -4,10 +4,8 @@ import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import jln.hobby.discordttsbot.lavaplayer.AudioPlayerSendHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ConcurrentReferenceHashMap;
 
 /**
  * 音声再生クラス関連のBean定義
@@ -20,10 +18,5 @@ public class PlayerConfiguration {
         AudioSourceManagers.registerRemoteSources(audioPlayerManager);
         audioPlayerManager.getConfiguration().setOutputFormat(StandardAudioDataFormats.DISCORD_OPUS);
         return audioPlayerManager;
-    }
-
-    @Bean
-    public ConcurrentReferenceHashMap<String, AudioPlayerSendHandler> audioPlayerSendHandlerMap() {
-        return new ConcurrentReferenceHashMap<>();
     }
 }
